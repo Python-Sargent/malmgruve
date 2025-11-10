9d0576d9f5f9eb951176fb4617a81b0c1eb20142
Python-Sargent <samuel@thesargents.org>
2025-11-09 22:10:02 -0600

    day one 11-9-25

---
+++ mods/mg_core/nodes.lua
@@ -0,0 +1,29 @@
+core.register_node("mg_core:soil", {
+	description = "Soil",
+	tiles = {"mg_soil.png"},
+	groups = {ground = 1, layer=0},
+})
+
+core.register_node("mg_core:limestone", {
+	description = "Limestone",
+	tiles = {"mg_limestone.png"},
+	groups = {ground = 1, layer=1},
+})
+
+core.register_node("mg_core:granite", {
+	description = "Granite",
+	tiles = {"mg_granite.png"},
+	groups = {ground = 1, layer=2},
+})
+
+core.register_node("mg_core:quartz", {
+	description = "Quartz",
+	tiles = {"mg_quartz.png"},
+	groups = {ground = 1, layer=3},
+})
+
+core.register_node("mg_core:peridotite", {
+	description = "Peridotite",
+	tiles = {"mg_Peridotite.png"},
+	groups = {ground = 1, layer=4},
+})
