768572e727278b74ddaea04972ec3ad7cf98cd19
Python-Sargent <samuel@thesargents.org>
2025-11-27 22:33:34 -0600

    fix_pick

--- mods/mg_core/tools.lua
+++ mods/mg_core/tools.lua
@@ -8,8 +8,6 @@ minetest.register_tool("mg_core:trusty_pick", {
 		groupcaps={
 			layer = {times={[4]=8.00, [3]=4.00, [2]=2.00, [1]=0.50}, uses=0},
 		},
-		damage_groups = {fleshy=4},
 	},
-	sound = {breaks = "default_tool_breaks"},
 	groups = {pickaxe = 1}
 })
